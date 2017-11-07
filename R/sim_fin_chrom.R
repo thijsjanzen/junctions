f3f166e093b97b447325184bdea4d6c66279e892
Thijs Janzen <janzen@evolbio.mpg.de>
2017-11-07 17:56:17 +0100

    added testthat support

--- R/sim_fin_chrom.R
+++ R/sim_fin_chrom.R
@@ -2,7 +2,7 @@ sim_fin_chrom <-
 function (pop_size, initial_heterozygosity, total_runtime, morgan, 
     seed, R) 
 {
-    .Call("_junctions_sim_inf_chrom", PACKAGE = "junctions", pop_size, 
+    .Call("_junctions_sim_fin_chrom", PACKAGE = "junctions", pop_size, 
         R, initial_heterozygosity, total_runtime, morgan, 
         seed)
 }
