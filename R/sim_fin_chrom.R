9cb5f28f7ab63a6f90dd9255d27cf679a92eeccb
Thijs Janzen <janzen@evolbio.mpg.de>
2017-11-08 18:04:44 +0100

    re-added sim_inf_chrom.R and sim_fin_chrom.R

---
+++ R/sim_fin_chrom.R
@@ -0,0 +1,10 @@
+sim_fin_chrom <- function (pop_size = 100,
+                           initial_heterozygosity = 0.5,
+                           total_runtime = 100,
+                           morgan = 1,
+                           seed = 42,
+                           R = 100) {
+    .Call("_junctions_sim_fin_chrom", PACKAGE = "junctions",
+        pop_size, initial_heterozygosity, total_runtime,
+        morgan, seed, R)
+}
---
+++ R/sim_inf_chrom.R
@@ -0,0 +1,10 @@
+sim_inf_chrom <- function(pop_size = 100,
+                          initial_heterozygosity = 0.5,
+                          total_runtime = 100,
+                          morgan = 1,
+                          markers = -1,
+                          seed = 42) {
+    .Call("_junctions_sim_inf_chrom", PACKAGE = "junctions",
+        pop_size, initial_heterozygosity, total_runtime,
+        morgan, markers, seed)
+}
