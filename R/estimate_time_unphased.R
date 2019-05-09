3e5849cce378729c23c5a91b83b8fe6a4e53c527
Thijs Janzen <thijsjanzen@gmail.com>
2019-05-09 12:28:30 +0200

    update manual

--- R/estimate_time_unphased.R
+++ R/estimate_time_unphased.R
@@ -5,6 +5,7 @@
 #' @param pop_size population size
 #' @param freq_ancestor_1 Frequency of ancestor 1 at t = 0
 #' @param max_t maximum time to be considered for maximum likelihood optimization. Too large values might cause the maximum likelihood algorithm to deviate from the global maximum.
+#' @param optim_pop_size If TRUE, population size is also optimized. Starting point of the optimizaton will then be on the given population size, and half the maximum time.
 #' @param verbose display intermediate output? Default = FALSE
 #' @export
 estimate_time_unphased <- function(local_anc,
@@ -12,6 +13,7 @@ estimate_time_unphased <- function(local_anc,
                                    pop_size,
                                    freq_ancestor_1,
                                    max_t,
+                                   optim_pop_size = FALSE,
                                    verbose = FALSE) {
 
   distances <- diff(locations)
@@ -29,14 +31,31 @@ estimate_time_unphased <- function(local_anc,
     return(focal_prob)
   }
 
-  to_optim <- function(t) {
-    local_probs <- mapply(calc_ll_single_state,
-                          local_states, distances,
-                          local_time = t,
-                          pop_size, freq_ancestor_1)
-    if(verbose) cat(t, -sum(local_probs), "\n")
-    return(-sum(local_probs))
+  if(optim_pop_size == FALSE) {
+
+    to_optim <- function(t) {
+      local_probs <- mapply(calc_ll_single_state,
+                            local_states, distances,
+                            local_time = t,
+                            pop_size, freq_ancestor_1)
+      if(verbose) cat(t, -sum(local_probs), "\n")
+      return(-sum(local_probs))
+    }
+    a1 <- stats::optimize(f = to_optim, interval = c(2, max_t))
+    return(a1)
+  } else {
+    to_optim <- function(params) {
+      if(params[[1]] < 2) return(-Inf)
+      if(params[[2]] < 2) return(-Inf)
+
+      local_probs <- mapply(calc_ll_single_state,
+                            local_states, distances,
+                            local_time = params[[1]],
+                            params[[2]], freq_ancestor_1)
+      if(verbose) cat("N",params[[1]],"t", params[[2]], -sum(local_probs), "\n")
+      return(-sum(local_probs))
+    }
+    a1 <- stats::optim(par = c(pop_size, max_t / 2), fn = to_optim)
+    return(a1)
   }
-  a1 <- stats::optimize(f = to_optim, interval = c(2, max_t))
-  return(a1)
 }
