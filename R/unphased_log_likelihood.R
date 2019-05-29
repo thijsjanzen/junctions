04ea2220d4d6cfad3ed636a7ad63a0521d9ba9b4
Thijs Janzen <thijsjanzen@gmail.com>
2019-05-29 09:44:37 +0200

    Update unphased_log_likelihood.R

--- R/unphased_log_likelihood.R
+++ R/unphased_log_likelihood.R
@@ -11,38 +11,73 @@ unphased_log_likelihood <- function(local_anc_matrix,
                                     locations,
                                     pop_size,
                                     freq_ancestor_1 = 0.5,
-                                    t) {
+                                    t,
+                                    test = FALSE) {
 
   distances <- diff(locations)
 
   local_anc <- local_anc_matrix
   if(is.matrix(local_anc_matrix)) {
     local_anc <- rep(3, length(local_anc_matrix[,1]))
-    homo_0 <- which(local_anc_matrix[,1] == 0 &
-                      (local_anc_matrix[,1] == local_anc_matrix[,2]))
-    homo_1 <- which(local_anc_matrix[,1] == 1 &
-                      (local_anc_matrix[,1] == local_anc_matrix[,2]))
-    local_anc[homo_0] <- 1
-    local_anc[homo_1] <- 2
+    homo_1 <- which(local_anc_matrix[, 1] == 0 &
+                      local_anc_matrix[, 2] == 0)
+    homo_2 <- which(local_anc_matrix[, 1] == 1 &
+                      local_anc_matrix[, 2] == 1)
+
+    local_anc[homo_1] <- 1
+    local_anc[homo_2] <- 2
+  }
+
+
+  labels <- (unique(local_anc))
+  if(sum( labels %in% 1:3) != length(labels)) {
+    stop("local ancestry labels should be [1, 2, 3] for homozygous anc 1, homozygous anc 2 and heterozygous\n")
   }
 
   to_analyze <- cbind(distances,
                       local_anc[1:(length(local_anc)-1)],
                       local_anc[2:length(local_anc)]      )
 
-  calc_ll <- function(local_time) {
+
+  calc_ll <- function(params) {
+
     local_probs <- apply(to_analyze, 1, get_cond_prob_vector,
                          freq_ancestor_1,
                          pop_size,
-                         local_time,
+                         local_time = params[[1]],
                          condition = TRUE)
 
     local_probs[1] <- get_cond_prob_vector(to_analyze[1,],
                                            freq_ancestor_1,
                                            pop_size,
-                                           local_time,
+                                           local_time = params[[1]],
                                            condition = FALSE)
-    return(sum(local_probs))
+
+    return(-sum(local_probs))
+  }
+
+  if(test == TRUE) {
+    calc_ll <- function(params) {
+
+      local_probs <- apply(to_analyze, 1, get_cond_prob_vector,
+                           freq_ancestor_1,
+                           pop_size,
+                           local_time = params[[1]],
+                           condition = FALSE)
+      return(-sum(local_probs))
+    }
+    if(length(t) == 1) {
+      focal_ll <- calc_ll(t)
+
+      return(focal_ll)
+    }
+    if(length(t) > 1) {
+      output <- c()
+      for(i in seq_along(t)) {
+        focal_ll[i] <- calc_ll(t)
+      }
+      return(output)
+    }
   }
 
   if(length(t) == 1) {
