7245cbedf4716d2324d1e3d4750c5186ad44f119
Thijs Janzen <thijsjanzen@gmail.com>
2020-11-26 18:24:20 +0100

    improve likelihood      cpp

--- src/estimate_time_unphased_cpp.cpp
+++ src/estimate_time_unphased_cpp.cpp
@@ -1,3 +1,13 @@
+
+#include <unistd.h>
+#include <vector>
+#include <cassert>
+#include <complex.h>
+#include <cmath>
+#include <iostream>
+
+#include "estimate_time_unphased_cpp.h"
+
 #include <Rcpp.h>
 using namespace Rcpp;
 
@@ -11,28 +21,38 @@ using namespace Rcpp;
 //   http://gallery.rcpp.org/
 //
 
+void force_output() {
+  // Rcout << s << "\n";
+  R_FlushConsole();
+  R_ProcessEvents();
+  R_CheckUserInterrupt();
+  ::sleep(1);
+}
+
+
 struct chromosome {
   std::vector< int > states;
   std::vector< double > distances;
 
   chromosome(const NumericMatrix& anc_matrix,
              const NumericVector& loc) {
-    states = std::vector<int>(3, loc.size());
-    distances = std::vector<double>(0.0, loc.size());
+    states = std::vector<int>(loc.size(), 2);
+    distances = std::vector<double>(loc.size(), 0.0);
+    //Rcout << states.size() << " " << distances.size() << "\n"; force_output();
 
-    for(int i = 0; i < loc.size(); ++i) {
+    for(int i = 0; i < distances.size(); ++i) {
+      // assert(i < distance.size());
+      // assert(i < anc_matrix.nrow());
       if (i > 0) distances[i] = loc[i] - loc[i - 1];
-      if (anc_matrix(i, 0) == anc_matrix(i, 1) && anc_matrix(i, 0) == 0) {
-        states[i] = 1 + anc_matrix(i, 0); // anc_matrix(i, 0) = [0, 1], labels are [1, 2]
+      if (anc_matrix(i, 0) == anc_matrix(i, 1)) {
+        states[i] = anc_matrix(i, 0); // anc_matrix(i, 0) = [0, 1]
       }
+      // Rcout << i << " " << states[i] << " " << distances[i] << "\n"; force_output();
     }
   }
 
   double calculate_likelihood(double t, int pop_size, double freq_ancestor_1);
 };
-
-
-
 
 
 // [[Rcpp::export]]
@@ -46,6 +66,7 @@ double estimate_time_unphased_cpp(const NumericMatrix& local_anc_matrix,
 
   chromosome focal_chrom(local_anc_matrix,
                          locations);
+  return 0.0;
 }
 
 //' function to calculate log likelihood using cpp
@@ -57,13 +78,17 @@ double estimate_time_unphased_cpp(const NumericMatrix& local_anc_matrix,
 //' @export
 // [[Rcpp::export]]
 double loglikelihood_unphased_cpp(const NumericMatrix& local_anc_matrix,
-                                   const NumericVector& locations,
-                                   int pop_size,
-                                   double freq_ancestor_1,
-                                   double t) {
+                                  const NumericVector& locations,
+                                  int pop_size,
+                                  double freq_ancestor_1,
+                                  double t) {
+
+  Rcout << "loading chromosome\n"; //force_output();
 
   chromosome focal_chrom(local_anc_matrix,
                          locations);
+
+  Rcout << "starting likelihood calculation\n"; force_output();
 
   double ll = focal_chrom.calculate_likelihood(t, pop_size, freq_ancestor_1);
   return ll;
@@ -118,37 +143,73 @@ void matrix_pow(std::vector< std::vector< double > >& trans_matrix,
 
 
 std::vector< double > calc_mult_matrix(const std::vector< std::vector< double > >& trans_matrix,
-                        int t) {
+                                       int t) {
 
   std::vector< std::vector< double > > out_matrix = trans_matrix;
   matrix_pow(out_matrix, t);
   return(out_matrix[0]);  // same as multiplying with {1, 0, 0, 0, 0, 0, 0}
 }
 
-std::vector< double > single_state(int t, int N, double d) {
-  std::vector<double> filler(7);
-  std::vector< std::vector< double > > trans_matrix(7, filler);
-  trans_matrix[0] = {1.0 - 1.0 / (2*N) - 2 * d , 2 * d, 0, 0, 0, 1.0 / (2*N), 0};
-  trans_matrix[1] = {1.0 / (2*N), 1 - 3 * 1.0 / (2*N) - d, d, 2 * 1.0 / (2*N), 0, 0, 0};
-  trans_matrix[2] = {0, 2 * 1.0 / (2*N), 1 - 4 * 1.0 / (2*N), 0, 2 * 1.0 /(2*N), 0, 0};
-  trans_matrix[3] = {0, 0, 0, 1 - 1.0 / (2*N) - d, d, 1.0 / (2*N), 0};
-  trans_matrix[4] = {0, 0, 0, 2 * 1.0 / (2*N), 1 - 3 * 1.0 / (2*N), 0, 1.0 / (2*N)};
-  trans_matrix[5] = {0, 0, 0, 0, 0, 1.0 - d, d};
-  trans_matrix[6] = {0 ,0, 0, 0, 0, 1.0 / (2*N),  1 - 1.0 / (2*N)};
 
-  std::vector<double> result = calc_mult_matrix(trans_matrix, t);
-  return(result);
+
+
+//' function to calculate 7 states
+//' @param t time
+//' @param N pop size
+//' @param d distance
+//' @export
+// [[Rcpp::export]]
+std::vector< double > single_state_cpp(int t, int N, double d) {
+
+  // I verified this with the synonymous R code, and it generates
+  // the correct answer.
+  // the cpp version is about 6 times faster:
+  //                                            expr    min      lq     mean  median      uq     max neval cld
+  // single_state(t = 100, N = 1000, d = 1e-05) 19.094 20.4415 22.72457 21.1695 21.9395 104.379   100   b
+  // single_state_cpp(t = 100, N = 1000, d = 1e-05)  3.236  3.7970  4.32687  4.0560  4.3925  14.303   100  a
+
+  double trans_matrix[7][7] =
+    {{1.0 - 1.0 / (2*N) - 2 * d , 2 * d, 0, 0, 0, 1.0 / (2*N), 0},
+    {1.0 / (2*N), 1 - 3 * 1.0 / (2*N) - d, d, 2 * 1.0 / (2*N), 0, 0, 0},
+    {0, 2 * 1.0 / (2*N), 1 - 4 * 1.0 / (2*N), 0, 2 * 1.0 /(2*N), 0, 0},
+    {0, 0, 0, 1 - 1.0 / (2*N) - d, d, 1.0 / (2*N), 0},
+    {0, 0, 0, 2 * 1.0 / (2*N), 1 - 3 * 1.0 / (2*N), 0, 1.0 / (2*N)},
+    {0, 0, 0, 0, 0, 1.0 - d, d},
+    {0 ,0, 0, 0, 0, 1.0 / (2*N),  1 - 1.0 / (2*N)}};
+
+
+  typedef SqMx<7, double> M7;
+  M7 m(trans_matrix);
+  auto m2 = m ^ t;
+  std::vector< double > output = m2();
+
+  return(output);
 }
 
-double get_prob_from_matrix(int left,
-                            int right,
-                            double p,
-                            const std::vector<double>& P) {
-// unphased probabilities!
-// left = marker on the left hand side [1 = PP, 2 = QQ, 3 = PQ or QP]
-// right = marker on the right hand side [1 = PP, 2 = QQ, 3 = PQ or QP]
-// p = frequency ancestor 1
-// P = states vector P_t^i
+
+
+//' function to calculate prob
+//' @param l left
+//' @param r right
+//' @param p freq
+//' @param P seven states output
+//' @export
+// [[Rcpp::export]]
+double get_prob_from_matrix_cpp(int left,
+                                int right,
+                                double p,
+                                const std::vector<double>& P_) {
+  // unphased probabilities!
+  // left = marker on the left hand side [1 = PP, 2 = QQ, 3 = PQ or QP]
+  // right = marker on the right hand side [1 = PP, 2 = QQ, 3 = PQ or QP]
+  // p = frequency ancestor 1
+  // P = states vector P_t^i
+
+  left++; // to conform to R notation. // code below is a close copy of R code
+  right++;
+
+  std::vector<double> P(1, 0);
+  P.insert(P.end(), P_.begin(), P_.end());
 
   double q  = 1 - p;
   double prob = 0;
@@ -209,6 +270,17 @@ double get_prob_from_matrix(int left,
 }
 
 
+
+//' function to calculate ll
+//' @param di distance
+//' @param l left
+//' @param r right
+//' @param t time
+//' @param pop_size population size
+//' @param freq_ancestor_1 freq ancestor
+//' @param condition conditioning
+//' @export
+// [[Rcpp::export]]
 double calc_ll(double di,
                double l,
                double r,
@@ -219,19 +291,27 @@ double calc_ll(double di,
   if (di < 0)
     return(-1e20);
 
-  std::vector< double > seven_states = single_state(t, pop_size, di);
+  std::vector< double > seven_states = single_state_cpp(t, pop_size, di);
+  for(int i = 0; i < seven_states.size(); ++i) {
+    Rcout << seven_states[i] << " ";
+  }
+  Rcout << "\n";
 
   std::vector< double > probs(3);
   double sum_prob = 0.0;
   for(int i = 0; i < 3; ++i) {
-    probs[i] = get_prob_from_matrix(l,
-                                    i,
-                                    freq_ancestor_1,
-                                    seven_states);
+    probs[i] = get_prob_from_matrix_cpp(l,
+                                        i,
+                                        freq_ancestor_1,
+                                        seven_states);
     sum_prob += probs[i];
+    Rcout << probs[i] << " ";
   }
+  Rcout << "\n";
 
   double focal_prob = probs[r];
+  Rcout << focal_prob << " " << sum_prob << "\n";
+
   if (condition == true)
     focal_prob *= 1.0 / sum_prob;
 
@@ -240,29 +320,33 @@ double calc_ll(double di,
 
 double chromosome::calculate_likelihood(double t, int pop_size, double freq_ancestor_1) {
 
- if (t < 1)
-   return(-1e20);
- if (pop_size < 2)
-   return(-1e20);
- if (freq_ancestor_1 >= 1)
-   return(-1e20);
- if (freq_ancestor_1 <= 0)
-   return(-1e20);
+  if (t < 1)
+    return(-1e20);
+  if (pop_size < 2)
+    return(-1e20);
+  if (freq_ancestor_1 >= 1)
+    return(-1e20);
+  if (freq_ancestor_1 <= 0)
+    return(-1e20);
 
 
- double di = distances[1];
- double l = states[0];
- double r = states[1];
+  double di = distances[1];
+  double l = states[0];
+  double r = states[1];
 
- double ll = calc_ll(di, l, r, t, pop_size, freq_ancestor_1, false);
+  //  double ll = calc_ll(di, l, r, t, pop_size, freq_ancestor_1, false);
 
-
-  for(int i = 2; i < distances.size(); ++i) {
+  std::vector<double> ll(distances.size());
+  ll[0] = calc_ll(di, l, r, t, pop_size, freq_ancestor_1, false);
+  for(int i = 1; i < distances.size(); ++i) {
+    //  Rcout << i << "\n"; force_output();
     di = distances[i];
     l = states[i - 1];
     r = states[i];
-    ll += calc_ll(di, l, r, t, pop_size, freq_ancestor_1, true);
+    ll[i] = calc_ll(di, l, r, t, pop_size, freq_ancestor_1, true);
+   // std::cout << di << " " << l << " " << r << " " << ll[i] << "\n";
   }
-  return(ll);
+
+  return(std::accumulate(ll.begin(), ll.end(), 0.0));
 }
 
---
+++ src/estimate_time_unphased_cpp.h
@@ -0,0 +1,72 @@
+#include <complex>
+#include <cmath>
+#include <iostream>
+
+//
+// from: https://rosettacode.org/wiki/Matrix-exponentiation_operator#C.2B.2B
+//
+
+template<int MSize = 7, class T = double >
+class SqMx {
+  typedef T Ax[MSize][MSize];
+  typedef SqMx<MSize, T> Mx;
+
+private:
+  Ax a;
+  SqMx() { }
+
+public:
+  SqMx(const Ax &_a) { // constructor with pre-defined array
+    for (int r = 0; r < MSize; r++)
+      for (int c = 0; c < MSize; c++)
+        a[r][c] = _a[r][c];
+  }
+
+  static Mx identity() {
+    Mx m;
+    for (int r = 0; r < MSize; r++)
+      for (int c = 0; c < MSize; c++)
+        m.a[r][c] = (r == c ? 1 : 0);
+    return m;
+  }
+
+  friend std::ostream &operator<<(std::ostream& os, const Mx &p)
+  { // ugly print
+    for (int i = 0; i < MSize; i++) {
+      for (int j = 0; j < MSize; j++)
+        os << p.a[i][j] << ',';
+      os << std::endl;
+    }
+    return os;
+  }
+
+  Mx operator*(const Mx &b) {
+    Mx d;
+    for (int r = 0; r < MSize; r++)
+      for (int c = 0; c < MSize; c++) {
+        d.a[r][c] = 0;
+        for (int k = 0; k < MSize; k++)
+          d.a[r][c] += a[r][k] * b.a[k][c];
+      }
+      return d;
+  }
+
+  Mx operator^(int n) {
+    if (n < 0)
+      throw "Negative exponent not implemented";
+
+    Mx d = identity();
+    for (Mx sq = *this; n > 0; sq = sq * sq, n /= 2)
+      if (n % 2 != 0)
+        d = d * sq;
+      return d;
+  }
+
+  std::vector<double> operator()() {
+    std::vector<double> output(MSize);
+    for(int i = 0; i < MSize; ++i) {
+      output[i] = static_cast<double>(a[0][i]);
+    }
+    return output;
+  }
+};
