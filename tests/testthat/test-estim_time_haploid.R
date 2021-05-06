25b45eecc90bd8ef1269816031f1d0fe188c2baa
Thijs Janzen <thijsjanzen@gmail.com>
2021-05-06 21:09:15 +0200

    Update test-estim_time_haploid.R

--- tests/testthat/test-estim_time_haploid.R
+++ tests/testthat/test-estim_time_haploid.R
@@ -5,7 +5,7 @@ test_that("estimate_time_haploid, use", {
                             time_points = sim_time,
                             pop_size = 1000,
                             seed = 1,
-                            num_indiv_sampled = 200)
+                            num_indiv_sampled = 20)
 
   estim_time <- estimate_time_haploid(cbind(vx$individual,
                                             vx$location,
@@ -13,7 +13,19 @@ test_that("estimate_time_haploid, use", {
                                       N = 1000,
                                       freq_ancestor_1 = 0.5)
 
-  testthat::expect_equal(estim_time$time, sim_time, tolerance = 0.1)
+  testthat::expect_equal(estim_time$time, sim_time, tolerance = 0.2)
+
+  focal_t <- 75:125
+  ll <- log_likelihood_haploid(ancestry_matrix = cbind(vx$individual,
+                                                       vx$location,
+                                                       vx$anc_chrom_1),
+                               N = 1000,
+                               freq_ancestor_1 = 0.5,
+                               t = focal_t)
+
+  a <- which.max(ll)
+  testthat::expect_equal(75 + a, 100, tolerance = 0.2)
+
 })
 
 test_that("estimate_time_diploid, use", {
