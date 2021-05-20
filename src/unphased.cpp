81b17753662f4513a125d1558094157c57544887
Thijs Janzen <thijsjanzen@gmail.com>
2021-05-20 11:20:19 +0200

    Update unphased.cpp

--- src/unphased.cpp
+++ src/unphased.cpp
@@ -20,7 +20,12 @@ void update_pop(const std::vector<Fish_inf>& old_pop,
                 double numRecombinations,
                 int num_threads) {
 
-  tbb::task_scheduler_init _tbb((num_threads > 0) ? num_threads : tbb::task_scheduler_init::automatic);
+//   tbb::task_scheduler_init _tbb((num_threads > 0) ? num_threads : tbb::task_scheduler_init::automatic);
+  auto nt = num_threads;
+  if (num_threads < 0) {
+    nt = tbb::task_scheduler_init::default_num_threads();
+  }
+  tbb::global_control gc(tbb::global_control::max_allowed_parallelism, nt);
 
   rnd_t rndgen;
 
